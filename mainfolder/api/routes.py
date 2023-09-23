e3477561fd032dfaea01feb63b6556a46cd719b9
karanja03 <wambuik03@gmail.com>
2023-09-23 14:57:20 +0300

    Added all routes

--- mainfolder/api/routes.py
+++ mainfolder/api/routes.py
@@ -2,6 +2,8 @@ from flask_restful import Resource
 from api.model import Restaurants, Restaurants_Pizzas, Pizzas
 from api.model import db
 from flask import session
+from flask import request
+
 
 class Get_Restaurants(Resource):
     def get(self):
@@ -66,6 +68,62 @@ class Get_Restaurants(Resource):
                 
 
                return {"error": "Restaurant not found"}, 404
+           
+class Get_Pizzas(Resource):
+    def get(self):
+        all_pizza=[]
+        
+        pizzas= Pizzas.query.all()
+        
+        for pizza in pizzas:
+            pizza_data={
+            'id': pizza.id,
+            'name': pizza.name ,
+            'ingredients':pizza.ingredients
+            }
+            
+            all_pizza.append(pizza_data)
+        
+        return all_pizza
+    
+class Post_RestaurantPizza(Resource):
+    def post (self ):
+        data= request.get_json()
+        price = data.get('price')
+        pizza_id = data.get('pizza_id')
+        restaurant_id = data.get('restaurant_id')
+        
+        if not price or not pizza_id or not restaurant_id:
+            return {'errors': ['[Lease enter the riht amounts]']}, 400
+        
+        pizza = Pizzas.query.get(pizza_id)
+        restaurant = Restaurants.query.get(restaurant_id)
+        
+        if not pizza or not restaurant:
+            return {'errors': ['Pizza or Restaurant not found']}, 404
+        
+        restaurant_pizza = Restaurants_Pizzas(
+            price=price,
+            pizza=pizza,
+            restaurant=restaurant
+        )
+        
+        db.session.add(restaurant_pizza)
+        db.session.commit()
+        
+        pizza_data={
+            'id': pizza.id,
+            'name': pizza.name,
+            'ingredients': pizza.ingredients
+            
+        }
+        
+        return pizza_data, 201
+
+
+        
+        
+        
             
         
      
