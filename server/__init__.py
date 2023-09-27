2c89f2f9aaeca760a274a5fad3fb181a5a0b6247
karanja03 <wambuik03@gmail.com>
2023-09-27 11:40:00 +0300

    added swagger

--- server/__init__.py
+++ server/__init__.py
@@ -4,12 +4,14 @@ from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from flask_marshmallow import Marshmallow
 from flask_swagger_ui import get_swaggerui_blueprint
+from flasgger import Swagger
 # from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 # Create a Flask application
 app = Flask(__name__)
 api = Api(app)
 ma = Marshmallow(app)
+swagger= Swagger(app)
 
 SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
 API_URL = 'http://petstore.swagger.io/v2/swagger.json'  # Our API url (can of course be a local resource)
@@ -19,7 +21,7 @@ swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
     API_URL,
     config={  # Swagger UI config overrides
-        'app_name': "Test application"
+        'app_name': "Restaurant API"
     },
     # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
     #    'clientId': "your-client-id",
@@ -31,7 +33,7 @@ swaggerui_blueprint = get_swaggerui_blueprint(
     # }
 )
 
-app.register_blueprint(swaggerui_blueprint)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 # Configure the database URI and disable modification tracking
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
